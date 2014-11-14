32d1b65ead72a7d0713793fac977ff1338109c06
moylop260 <moylop260@vauxoo.com>
2014-11-13 21:32:14 -0600

    [REF] auto-fix conventions

--- mrp_webkit/report/order.py
+++ mrp_webkit/report/order.py
@@ -26,6 +26,7 @@ from openerp.report import report_sxw
 
 
 class order(report_sxw.rml_parse):
+
     def __init__(self, cr, uid, name, context):
         super(order, self).__init__(cr, uid, name, context=context)
         self.localcontext.update({

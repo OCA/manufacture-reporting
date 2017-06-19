81ce4103e43fc92a73ce46a95cf1c198bfa04ea7
mreficent <miquel.raich@eficent.com>
2017-06-19 18:03:04 +0200

    [MIG] mrp_bom_structure_xlsx: Migration to 10.0

--- mrp_bom_structure_xlsx/__manifest__.py
+++ mrp_bom_structure_xlsx/__manifest__.py
@@ -5,8 +5,9 @@
 
 {
     'name': "Export BOM Structure to Excel .XLSX",
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Manufacturing',
+    'summary': 'Export Bill of Materials Structure',
     'author': "Eficent, Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/manufacture-reporting',
     'license': 'AGPL-3',

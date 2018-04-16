6cd47d7c633bdf02574f20cb156f208b668c310d
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2018-04-16 16:31:05 -0500

    [ADD] mrp_order_report_stock_location

---
+++ mrp_order_report_stock_location/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2018 Open Source Integrators
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock Location on the Production Order",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Open Source Integrators,"
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/manufacture-reporting",
+    "category": "Manufacturing",
+    "depends": [
+        "mrp",
+    ],
+    "data": [
+        "views/mrp_order_report_stock_location.xml",
+    ],
+}

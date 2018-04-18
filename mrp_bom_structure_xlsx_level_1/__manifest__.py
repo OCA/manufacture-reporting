6042f9dc53daa17b74c1539dbcbc8a8764c460ce
hveficent <hector.villarreal@eficent.com>
2018-04-18 16:56:50 +0200

    [MIG] mrp_bom_structure_xlsx_level_1: Migration to 11.0

--- mrp_bom_structure_xlsx_level_1/__manifest__.py
+++ mrp_bom_structure_xlsx_level_1/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': "Export BOM Structure (Level 1) to Excel .XLSX",
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Manufacturing',
     'author': "Eficent, Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/manufacture-reporting',

826e889e564a1e53f9400125bd30d2fea5f2e3e4
Lois Rilo <lois.rilo@eficent.com>
2018-11-05 11:40:11 +0100

    [11.0][IMP] mrp_flattened_bom_xlsx: cover multi level bom case and add tests (#48)

--- mrp_flattened_bom_xlsx/models/mrp_bom.py
+++ mrp_flattened_bom_xlsx/models/mrp_bom.py
@@ -2,34 +2,43 @@
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models
+from odoo import api, models
 
 
 class MrpBom(models.Model):
     """ Defines bills of material for a product or a product template """
     _inherit = 'mrp.bom'
 
+    @api.multi
     def _get_flattened_totals(self, factor=1, totals=None):
+        """Calculate the **unitary** product requirements of flattened BOM.
+        *Unit* means that the requirements are computed for one unit of the
+        default UoM of the product.
+        :returns: dict: keys are components and values are aggregated quantity
+        in the product default UoM.
         """
-        Generate a summary of product quantities as a dict of flattened BOM
-        """
+        self.ensure_one()
         if totals is None:
             totals = {}
+        factor /= self.product_uom_id._compute_quantity(
+            self.product_qty, self.product_tmpl_id.uom_id, round=False)
         for line in self.bom_line_ids:
             sub_bom = self._bom_find(product=line.product_id)
             if sub_bom:
-                factor *= line.product_uom_id._compute_quantity(
-                    line.product_qty, line.product_id.uom_id)
-                sub_bom._get_flattened_totals(factor, totals)
+                new_factor = factor * line.product_uom_id._compute_quantity(
+                    line.product_qty, line.product_id.uom_id, round=False)
+                sub_bom._get_flattened_totals(new_factor, totals)
             else:
-                factor /= self.product_uom_id._compute_quantity(
-                    self.product_qty, self.product_uom_id)
                 if totals.get(line.product_id):
-                    totals[line.product_id] += line.product_uom_id. \
-                        _compute_quantity(
-                        line.product_qty * factor, line.product_id.uom_id)
+                    totals[line.product_id] += factor * \
+                        line.product_uom_id._compute_quantity(
+                            line.product_qty,
+                            line.product_id.uom_id,
+                            round=False)
                 else:
-                    totals[line.product_id] = line.product_uom_id. \
-                        _compute_quantity(
-                        line.product_qty * factor, line.product_id.uom_id)
+                    totals[line.product_id] = factor * \
+                        line.product_uom_id._compute_quantity(
+                            line.product_qty,
+                            line.product_id.uom_id,
+                            round=False)
         return totals

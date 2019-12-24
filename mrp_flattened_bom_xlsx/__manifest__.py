9618ef60d964270615eb5bd5688aa857846d3b34
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2019-12-24 12:07:24 +0100

    [IMP] mrp_flattened_bom_xlsx: black, isort

--- mrp_flattened_bom_xlsx/__manifest__.py
+++ mrp_flattened_bom_xlsx/__manifest__.py
@@ -3,15 +3,13 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': "Export Flattened BOM to Excel",
-    'version': '12.0.1.0.0',
-    'category': 'Manufacturing',
-    'author': "Eficent, Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/manufacture-reporting',
-    'license': 'AGPL-3',
-    'depends': ['report_xlsx', 'mrp'],
-    'data': [
-        'report/flattened_bom_xlsx.xml',
-    ],
-    'installable': True
+    "name": "Export Flattened BOM to Excel",
+    "version": "12.0.1.0.0",
+    "category": "Manufacturing",
+    "author": "Eficent, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/manufacture-reporting",
+    "license": "AGPL-3",
+    "depends": ["report_xlsx", "mrp"],
+    "data": ["report/flattened_bom_xlsx.xml"],
+    "installable": True,
 }

6f0ac7cfc0555c208273dba26de8147029775155
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2019-12-24 12:07:24 +0100

    [IMP] mrp_flattened_bom_xlsx: black, isort

--- mrp_flattened_bom_xlsx/models/mrp_bom.py
+++ mrp_flattened_bom_xlsx/models/mrp_bom.py
@@ -7,7 +7,8 @@ from odoo import api, models
 
 class MrpBom(models.Model):
     """ Defines bills of material for a product or a product template """
-    _inherit = 'mrp.bom'
+
+    _inherit = "mrp.bom"
 
     @api.multi
     def _get_flattened_totals(self, factor=1, totals=None):
@@ -21,24 +22,28 @@ class MrpBom(models.Model):
         if totals is None:
             totals = {}
         factor /= self.product_uom_id._compute_quantity(
-            self.product_qty, self.product_tmpl_id.uom_id, round=False)
+            self.product_qty, self.product_tmpl_id.uom_id, round=False
+        )
         for line in self.bom_line_ids:
             sub_bom = self._bom_find(product=line.product_id)
             if sub_bom:
                 new_factor = factor * line.product_uom_id._compute_quantity(
-                    line.product_qty, line.product_id.uom_id, round=False)
+                    line.product_qty, line.product_id.uom_id, round=False
+                )
                 sub_bom._get_flattened_totals(new_factor, totals)
             else:
                 if totals.get(line.product_id):
-                    totals[line.product_id] += factor * \
-                        line.product_uom_id._compute_quantity(
-                            line.product_qty,
-                            line.product_id.uom_id,
-                            round=False)
+                    totals[line.product_id] += (
+                        factor
+                        * line.product_uom_id._compute_quantity(
+                            line.product_qty, line.product_id.uom_id, round=False
+                        )
+                    )
                 else:
-                    totals[line.product_id] = factor * \
-                        line.product_uom_id._compute_quantity(
-                            line.product_qty,
-                            line.product_id.uom_id,
-                            round=False)
+                    totals[line.product_id] = (
+                        factor
+                        * line.product_uom_id._compute_quantity(
+                            line.product_qty, line.product_id.uom_id, round=False
+                        )
+                    )
         return totals
--- mrp_flattened_bom_xlsx/tests/test_flattened_bom.py
+++ mrp_flattened_bom_xlsx/tests/test_flattened_bom.py
@@ -5,94 +5,90 @@ import odoo.tests.common as common
 
 
 class TestFlattenedBom(common.SavepointCase):
-
     @classmethod
     def setUpClass(cls):
         super(TestFlattenedBom, cls).setUpClass()
 
-        cls.product_obj = cls.env['product.product']
-        cls.bom_obj = cls.env['mrp.bom']
-        cls.bom_line_obj = cls.env['mrp.bom.line']
+        cls.product_obj = cls.env["product.product"]
+        cls.bom_obj = cls.env["mrp.bom"]
+        cls.bom_line_obj = cls.env["mrp.bom.line"]
 
-        cls.uom_dozen = cls.env.ref('uom.product_uom_dozen')
+        cls.uom_dozen = cls.env.ref("uom.product_uom_dozen")
 
         # Create products:
-        cls.product_top = cls.product_obj.create({
-            'name': 'Final Product',
-            'type': 'product',
-            'standard_price': 300.0,
-        })
-        cls.product_sub_1 = cls.product_obj.create({
-            'name': 'L01-01',
-            'type': 'product',
-            'standard_price': 300.0,
-        })
-        cls.product_sub_2 = cls.product_obj.create({
-            'name': 'L01-02',
-            'type': 'product',
-            'standard_price': 300.0,
-        })
-        cls.component_1 = cls.product_obj.create({
-            'name': 'RM 01',
-            'type': 'product',
-            'standard_price': 100.0,
-        })
-        cls.component_2 = cls.product_obj.create({
-            'name': 'RM 01',
-            'type': 'product',
-            'standard_price': 75.0,
-        })
-        cls.component_3 = cls.product_obj.create({
-            'name': 'RM 03',
-            'type': 'product',
-            'standard_price': 75.0,
-        })
+        cls.product_top = cls.product_obj.create(
+            {"name": "Final Product", "type": "product", "standard_price": 300.0}
+        )
+        cls.product_sub_1 = cls.product_obj.create(
+            {"name": "L01-01", "type": "product", "standard_price": 300.0}
+        )
+        cls.product_sub_2 = cls.product_obj.create(
+            {"name": "L01-02", "type": "product", "standard_price": 300.0}
+        )
+        cls.component_1 = cls.product_obj.create(
+            {"name": "RM 01", "type": "product", "standard_price": 100.0}
+        )
+        cls.component_2 = cls.product_obj.create(
+            {"name": "RM 01", "type": "product", "standard_price": 75.0}
+        )
+        cls.component_3 = cls.product_obj.create(
+            {"name": "RM 03", "type": "product", "standard_price": 75.0}
+        )
 
         # Create Bills of Materials:
-        cls.bom_top = cls.bom_obj.create({
-            'product_tmpl_id': cls.product_top.product_tmpl_id.id,
+        cls.bom_top = cls.bom_obj.create(
+            {"product_tmpl_id": cls.product_top.product_tmpl_id.id}
+        )
+        cls.line_top_1 = cls.bom_line_obj.create(
+            {
+                "product_id": cls.product_sub_1.id,
+                "bom_id": cls.bom_top.id,
+                "product_qty": 2.0,
+            }
+        )
+        cls.line_top_2 = cls.bom_line_obj.create(
+            {
+                "product_id": cls.product_sub_2.id,
+                "bom_id": cls.bom_top.id,
+                "product_qty": 5.0,
+            }
+        )
 
-        })
-        cls.line_top_1 = cls.bom_line_obj.create({
-            'product_id': cls.product_sub_1.id,
-            'bom_id': cls.bom_top.id,
-            'product_qty': 2.0,
-        })
-        cls.line_top_2 = cls.bom_line_obj.create({
-            'product_id': cls.product_sub_2.id,
-            'bom_id': cls.bom_top.id,
-            'product_qty': 5.0,
-        })
+        cls.bom_sub_1 = cls.bom_obj.create(
+            {"product_tmpl_id": cls.product_sub_1.product_tmpl_id.id}
+        )
+        cls.line_sub_1_1 = cls.bom_line_obj.create(
+            {
+                "product_id": cls.component_1.id,
+                "bom_id": cls.bom_sub_1.id,
+                "product_qty": 2.0,
+            }
+        )
+        cls.line_sub_1_2 = cls.bom_line_obj.create(
+            {
+                "product_id": cls.component_2.id,
+                "bom_id": cls.bom_sub_1.id,
+                "product_qty": 5.0,
+            }
+        )
 
-        cls.bom_sub_1 = cls.bom_obj.create({
-            'product_tmpl_id': cls.product_sub_1.product_tmpl_id.id,
-
-        })
-        cls.line_sub_1_1 = cls.bom_line_obj.create({
-            'product_id': cls.component_1.id,
-            'bom_id': cls.bom_sub_1.id,
-            'product_qty': 2.0,
-        })
-        cls.line_sub_1_2 = cls.bom_line_obj.create({
-            'product_id': cls.component_2.id,
-            'bom_id': cls.bom_sub_1.id,
-            'product_qty': 5.0,
-        })
-
-        cls.bom_sub_2 = cls.bom_obj.create({
-            'product_tmpl_id': cls.product_sub_2.product_tmpl_id.id,
-
-        })
-        cls.line_sub_2_1 = cls.bom_line_obj.create({
-            'product_id': cls.component_1.id,
-            'bom_id': cls.bom_sub_2.id,
-            'product_qty': 3.0,
-        })
-        cls.line_sub_2_2 = cls.bom_line_obj.create({
-            'product_id': cls.component_3.id,
-            'bom_id': cls.bom_sub_2.id,
-            'product_qty': 3.0,
-        })
+        cls.bom_sub_2 = cls.bom_obj.create(
+            {"product_tmpl_id": cls.product_sub_2.product_tmpl_id.id}
+        )
+        cls.line_sub_2_1 = cls.bom_line_obj.create(
+            {
+                "product_id": cls.component_1.id,
+                "bom_id": cls.bom_sub_2.id,
+                "product_qty": 3.0,
+            }
+        )
+        cls.line_sub_2_2 = cls.bom_line_obj.create(
+            {
+                "product_id": cls.component_3.id,
+                "bom_id": cls.bom_sub_2.id,
+                "product_qty": 3.0,
+            }
+        )
 
     def test_01_flattened_totals(self):
         """Test totals computation with a multi level BoM."""

4be32b13413a600305555f172de9412818214609
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-01-08 10:33:39 +0100

    [MIG] mrp_bom_structure_report_level_1: Migration to 13.0

--- mrp_bom_structure_report_level_1/__manifest__.py
+++ mrp_bom_structure_report_level_1/__manifest__.py
@@ -1,11 +1,10 @@
-# © 2017 Eficent Business and IT Consulting Services S.L.
-#        (http://www.eficent.com)
+# © Copyright 2017-19 ForgeFlow S.L. (https://www.forgeflow.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "MRP BOM Structure Report Level 1",
-    "version": "12.0.1.0.0",
-    "author": "Eficent, Odoo Community Association (OCA)",
+    "version": "13.0.1.0.0",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/manufacture-reporting",
     "category": "Manufacture",
     "depends": ["mrp"],

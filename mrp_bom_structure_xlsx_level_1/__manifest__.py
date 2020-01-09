bd5f3932a5e28e3dad8ed70eb5ab7896090dc48b
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-01-09 12:02:51 +0100

    [IMP] mrp_bom_structure_xlsx_level_1: black, isort

--- mrp_bom_structure_xlsx_level_1/__manifest__.py
+++ mrp_bom_structure_xlsx_level_1/__manifest__.py
@@ -3,16 +3,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': "MRP BOM Structure XLSX Level 1",
-    'version': '12.0.1.0.0',
-    'category': 'Manufacturing',
-    'summary': "Export BOM Structure (Level 1) to Excel .XLSX",
-    'author': "Eficent, Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/manufacture-reporting',
-    'license': 'AGPL-3',
-    "depends": ['mrp_bom_structure_xlsx'],
-    "data": [
-        'report/bom_structure_xlsx.xml',
-    ],
-    "installable": True
+    "name": "MRP BOM Structure XLSX Level 1",
+    "version": "12.0.1.0.0",
+    "category": "Manufacturing",
+    "summary": "Export BOM Structure (Level 1) to Excel .XLSX",
+    "author": "Eficent, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/manufacture-reporting",
+    "license": "AGPL-3",
+    "depends": ["mrp_bom_structure_xlsx"],
+    "data": ["report/bom_structure_xlsx.xml"],
+    "installable": True,
 }

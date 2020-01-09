edbdfe18310473e2d61067ae1f901051dc512707
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-01-09 12:14:18 +0100

    [MIG] mrp_bom_structure_xlsx_level_1: Migration to 13.0
    [UPD] Update mrp_bom_structure_xlsx_level_1.pot
    
    [UPD] README.rst

--- mrp_bom_structure_xlsx_level_1/__manifest__.py
+++ mrp_bom_structure_xlsx_level_1/__manifest__.py
@@ -1,13 +1,12 @@
-# Copyright 2018 Eficent Business and IT Consulting Services S.L.
-#   (http://www.eficent.com)
+# Copyright 2018-19 ForgeFlow S.L. (https://www.forgeflow.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "MRP BOM Structure XLSX Level 1",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Manufacturing",
     "summary": "Export BOM Structure (Level 1) to Excel .XLSX",
-    "author": "Eficent, Odoo Community Association (OCA)",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/manufacture-reporting",
     "license": "AGPL-3",
     "depends": ["mrp_bom_structure_xlsx"],

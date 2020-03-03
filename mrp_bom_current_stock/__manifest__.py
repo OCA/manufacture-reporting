979546422473f2a2fab58ef4d6f2b9ffbb1c9f48
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-03-03 15:27:55 +0100

    [IMP] mrp_bom_current_stock: black, isort

--- mrp_bom_current_stock/__manifest__.py
+++ mrp_bom_current_stock/__manifest__.py
@@ -5,16 +5,13 @@
 {
     "name": "MRP BoM Current Stock",
     "summary": "Add a report that explodes the bill of materials and show the "
-               "stock available in the source location.",
+    "stock available in the source location.",
     "version": "12.0.1.0.0",
     "category": "Manufacture",
     "website": "https://github.com/OCA/manufacture-reporting",
     "author": "Eficent, Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "depends": [
-        "mrp_bom_location",
-        "report_xlsx",
-    ],
+    "depends": ["mrp_bom_location", "report_xlsx"],
     "data": [
         "reports/report_mrpcurrentstock.xml",
         "wizard/bom_route_current_stock_view.xml",

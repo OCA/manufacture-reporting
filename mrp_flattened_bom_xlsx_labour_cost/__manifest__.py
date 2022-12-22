0b8280fe7a7eb75f8170da0079238522631b2566
BernatPForgeFlow <bernat.puig@forgeflow.com>
2022-12-22 17:27:38 +0100

    [14.0][ADD] mrp_flattened_bom_labour_cost

---
+++ mrp_flattened_bom_xlsx_labour_cost/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 ForgeFlow S.L. (http://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Export Flattened BOM to Excel with labour cost",
+    "version": "14.0.1.0.0",
+    "category": "Manufacturing",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/manufacture-reporting",
+    "license": "AGPL-3",
+    "depends": ["mrp_flattened_bom_xlsx_direct_materials_cost", "mrp"],
+    "installable": True,
+}

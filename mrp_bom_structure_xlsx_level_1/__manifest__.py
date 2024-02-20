7b439bbb254911030e3a7a63ed390a3643ad23a7
Víctor Martínez <victor.martinez@tecnativa.com>
2024-02-20 09:49:51 +0100

    [MIG] mrp_bom_structure_xlsx_level_1: Migration to 16.0
    TT47840

--- mrp_bom_structure_xlsx_level_1/__manifest__.py
+++ mrp_bom_structure_xlsx_level_1/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "MRP BOM Structure XLSX Level 1",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Manufacturing",
     "summary": "Export BOM Structure (Level 1) to Excel .XLSX",
     "author": "ForgeFlow, Odoo Community Association (OCA)",

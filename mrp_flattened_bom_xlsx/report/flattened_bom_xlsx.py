d090e3d6a37006b42a9ec9bd1ce73d9a2fbb6f37
Nico Darnis <nico@evoeron.com>
2025-01-31 13:11:52 +0100

    ✨ Separate each Export by sheet
    - In case of multiple BOM export each export will have it's own sheet.
    Added the company color as a header and computed the text color based on contrast
    - Added also the company name of the bom as well as the time for export for versionning and sharing possibilities
    - Renamed the file to the ref of the BOM

--- mrp_flattened_bom_xlsx/report/flattened_bom_xlsx.py
+++ mrp_flattened_bom_xlsx/report/flattened_bom_xlsx.py
@@ -1,15 +1,26 @@
-# Copyright 2018 ForgeFlow S.L.
-#   (http://www.forgeflow.com)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 from odoo import models
 from odoo.tools.translate import _
-
+from datetime import datetime
 
 class FlattenedBomXlsx(models.AbstractModel):
     _name = "report.mrp_flattened_bom_xlsx.flattened_bom_xlsx"
     _inherit = "report.report_xlsx.abstract"
     _description = "Flattened BOM XLSX"
+
+    def get_text_color(self, hex_color):
+        """Determine if text should be black or white based on background color luminance.
+        See https://www.w3.org/TR/WCAG21/#dfn-relative-luminance """
+        hex_color = hex_color.lstrip("#")
+        if len(hex_color) != 6:
+            return "#000000"
+
+        # Convert hex to linear RGB
+        linear_rgb = []
+        for i in range(0, 6, 2):
+            value = int(hex_color[i:i+2], 16) / 255.0
+            linear_rgb.append(value / 12.92 if value <= 0.04045 else ((value + 0.055) / 1.055) ** 2.4)
+        luminance = 0.2126 * linear_rgb[0] + 0.7152 * linear_rgb[1] + 0.0722 * linear_rgb[2]
+        return "#FFFFFF" if luminance < 0.179 else "#000000"
 
     def print_flattened_bom_lines(self, bom, requirements, sheet, row):
         i = row
@@ -31,36 +42,50 @@ class FlattenedBomXlsx(models.AbstractModel):
 
     def generate_xlsx_report(self, workbook, data, objects):
         workbook.set_properties(
-            {"comments": "Created with Python and XlsxWriter from Odoo 11.0"}
+            {"comments": "Created with Python and XlsxWriter from Odoo"}
         )
-        sheet = workbook.add_worksheet(_("Flattened BOM"))
-        sheet.set_landscape()
-        sheet.fit_to_pages(1, 0)
-        sheet.set_zoom(80)
-        sheet.set_column(0, 0, 40)
-        sheet.set_column(1, 2, 20)
-        sheet.set_column(3, 3, 40)
-        sheet.set_column(4, 6, 20)
-        title_style = workbook.add_format(
-            {"bold": True, "bg_color": "#FFFFCC", "bottom": 1}
-        )
-        sheet_title = [
-            _("BOM Name"),
-            _("Product Reference"),
-            _("Product Name"),
-            _("Quantity"),
-            _("Unit of Measure"),
-            _("Reference"),
-        ]
-        sheet.set_row(0, None, None, {"collapsed": 1})
-        sheet.write_row(1, 0, sheet_title, title_style)
-        sheet.freeze_panes(2, 0)
-        i = 2
 
-        for o in objects:
-            # We need to calculate the totals for the BoM qty and UoM:
-            starting_factor = o.product_uom_id._compute_quantity(
-                o.product_qty, o.product_tmpl_id.uom_id, round=False
+        for bom in objects:
+            export_date = datetime.now()
+            bg_color = bom.company_id.secondary_color or "#FFFFCC"
+            text_color = self.get_text_color(bg_color)
+
+            title_style = workbook.add_format(
+                {"bold": True, "bg_color": bg_color, "font_color": text_color, "bottom": 1}
             )
-            totals = o._get_flattened_totals(factor=starting_factor)
-            i = self.print_flattened_bom_lines(o, totals, sheet, i)
+
+            sheet_name = bom.code or _("BOM")
+            sheet = workbook.add_worksheet(sheet_name[:31])  # Limit to Excel's max 31 chars
+            sheet.set_landscape()
+            sheet.fit_to_pages(1, 0)
+            sheet.set_zoom(100)
+            sheet.set_column(0, 0, 40)
+            sheet.set_column(1, 5, 20)
+            sheet.set_column(3, 4, 10)
+
+            # Company Info
+            company_name =  bom.company_id.partner_id.contact_address_inline or bom.company_id.name or ""
+            header_text = f"© {company_name}"
+
+            sheet.write(0, 0, header_text)
+            sheet.write(0, 4, str(export_date))
+
+            # Table Headers
+            sheet_title = [
+                _("BOM Name"),
+                _("Product Reference"),
+                _("Product Name"),
+                _("Quantity"),
+                _("Unit of Measure"),
+                _("Reference"),
+            ]
+            sheet.write_row(1, 0, sheet_title, title_style)
+            sheet.freeze_panes(2, 0)
+
+            # Populate BoM Data
+            i = 2
+            starting_factor = bom.product_uom_id._compute_quantity(
+                bom.product_qty, bom.product_tmpl_id.uom_id, round=False
+            )
+            totals = bom._get_flattened_totals(factor=starting_factor)
+            self.print_flattened_bom_lines(bom, totals, sheet, i)

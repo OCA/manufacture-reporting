0790a16691b7556f63289ffcecd3019b2ef351c5
Iván Todorovich <ivan.todorovich@gmail.com>
2025-02-19 15:30:01 -0300

    [ADD] mrp_order_report_reserved

---
+++ mrp_order_report_reserved/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2025 Camptocamp SA (https://www.camptocamp.com).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Display Component's Reserved Quantity on the Production Order Report",
+    "version": "16.0.1.0.0",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/manufacture-reporting",
+    "license": "AGPL-3",
+    "maintainers": ["ivantodorovich"],
+    "category": "Manufacturing",
+    "depends": ["mrp"],
+    "data": ["reports/mrp_order_report.xml"],
+}
